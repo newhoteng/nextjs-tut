cbb5a0a727d9079517cacae4087d0f4719072b15
Harriet Oteng <harriet.oteng@gmail.com>
2023-11-09 10:12:20 +0000

    Add validation to edit-form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -81,14 +81,24 @@ export async function createInvoice(prevState: State, formData: FormData) {
   // console.log(rawFormData);
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
+
   try {
     await sql`
       UPDATE invoices
